0fbb510eeb03e819b02d23428edf25a512021e23
lawcia <lawrencianjume@gmail.com>
2019-12-16 14:43:36 +0000

    add more basic testing and imported router

--- quizilla/src/App.js
+++ quizilla/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Link, 
+  Switch
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>   
+      <div className="App">
+        <p>Hello World!</p>
+      </div>
+    </Router>
   );
 }
 
--- quizilla/src/App.test.js
+++ quizilla/src/App.test.js
@@ -4,12 +4,18 @@ import {shallow} from 'enzyme';
 import App from './App';
 
 describe('App.js', () => {
-  it('should render div', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.contains(<div className="App" />)).to.equal(true);
+  let wrapper;
+
+  beforeEach(() => {
+     wrapper = shallow(<App />);
   })
 
+  it('contains only one element with classname App', () => {
+    expect(wrapper.find('.App')).to.have.lengthOf(1);
+  })
+
+  // app is not an empty div
   it('should have no other components', () => {
-    
+    expect(wrapper.find('.App')).to.not.be.blank();
   })
 })
