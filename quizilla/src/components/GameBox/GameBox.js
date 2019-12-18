22ee374864b2517ed661a6d5f5fe5a0c22e234e1
lawcia <lawrencianjume@gmail.com>
2019-12-18 15:43:25 +0000

    add html parser

--- quizilla/src/components/GameBox/GameBox.js
+++ quizilla/src/components/GameBox/GameBox.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import {Parser} from 'html-to-react';  
 
 const GameBox = (props) => {
+    const formatQuestion = () => {
+        let question = props.gameInfo.question
+        const htmlToReactParser = new Parser();
+        const reactElement = htmlToReactParser.parse(question)
+        return reactElement
+    }
+
     let answers = [...props.gameInfo.incorrect_answers, props.gameInfo.correct_answer].sort();  
-    
     const onClickHandler = (event) => {
-        // console.log(event.target.value);
         props.calculateScore(event.target.value);
     }
 
     return(
         <div>
-            <h3>{props.gameInfo.question}</h3>
-            
+            <h3>{formatQuestion()}</h3>
             {answers.map(answer => {
-                return <button value = {answer} id = {answer} onClick={(event)=> onClickHandler(event)}>{answer}</button>
+                const htmlToReactParser2 = new Parser();
+                return <button value = {answer} id = {answer} onClick={(event)=> onClickHandler(event)}>{htmlToReactParser2.parse(answer)}</button>
             })}
         </div>
     )
