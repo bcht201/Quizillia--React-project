a9b8e603d6c94b04ec65740cdf612df1601c7a61
bcht201 <brianting1227@gmail.com>
2019-12-20 08:59:18 +0000

    fix quiz testing

--- quizilla/src/components/Quiz/Quiz.js
+++ quizilla/src/components/Quiz/Quiz.js
@@ -28,28 +28,57 @@ class Quiz extends React.Component{
     apiCall = () => {
         axios.get(`https://opentdb.com/api.php?amount=${this.props.maxNumberOfQuestions}&category=${this.props.catID}&difficulty=${this.props.diff}`)
         .then(response => {
-            console.log(response.data.results)
             this.setState({data: response.data.results})})
     }
 
-    calculateScore = answer =>{
-        console.log("I calculated the score");
-        let points;
+    // calculateScore = answer =>{
+    //     let points;
+    //     if(answer === this.state.data[this.state.index].correct_answer){
+    //         points = 1;
+    //     } else{
+    //         points = -1;
+    //     }
+    //     this.setState(prevState => {
+    //         let newScore = [...prevState.score];
+    //         let questionIncrement;
+    //         if(this.state.numberOfPlayers === 1) {
+    //             questionIncrement = 1;
+    //         } else {
+    //             questionIncrement = prevState.counter % this.state.numberOfPlayers
+    //         }
+    //         newScore[this.state.counter % this.state.numberOfPlayers] += points;
+    //         return ({score: newScore, counter: prevState.counter + 1, index: prevState.index + questionIncrement})})
+    // }
+
+    nextGameState = answer =>{
+        let newScore = this.calculateScore(answer);
+        let nextQuestion = this.updateQuestion();
+        this.setState({score : newScore, counter: this.state.counter + 1, index: this.state.index + nextQuestion});
+    }
+
+    calculateScore = (answer) =>{
+        let points = this.state.score;
+        let currentPlayer = this.state.counter % this.state.numberOfPlayers;
         if(answer === this.state.data[this.state.index].correct_answer){
-            points = 1;
+            points[currentPlayer] += 1;
         } else{
-            points = -1;
+            points[currentPlayer] += -1;
         }
-        this.setState(prevState => {
-            let newScore = [...prevState.score];
-            let questionIncrement;
-            if(this.state.numberOfPlayers === 1) {
-                questionIncrement = 1;
-            } else {
-                questionIncrement = prevState.counter % this.state.numberOfPlayers
-            }
-            newScore[this.state.counter % this.state.numberOfPlayers] += points;
-            return ({score: newScore, counter: prevState.counter + 1, index: prevState.index + questionIncrement})})
+        return points;
+    };
+
+    updateQuestion = () =>{
+        let questionIncrement;
+        if(this.state.numberOfPlayers === 1) {
+            questionIncrement = 1;
+        } else {
+            questionIncrement = this.state.counter % this.state.numberOfPlayers
+        }
+        return questionIncrement;
+    }
+
+    updateScore = () =>{
+
     }
 
     gameState = () => {
@@ -65,9 +94,10 @@ class Quiz extends React.Component{
     }
 
     generateResults = () => {
-        let emojis = [<p>&#128563;</p>, <p>&#128513;</p> ];
         return this.state.score.map((score, index)=> {
-            return(<p className = "resultsMsg"> Player {index+1} got {score}/{this.props.maxNumberOfQuestions} {score < 0? emojis[0] : emojis[1]} </p>)
+            return(
+                <p className = "resultsMsg"> Player {index+1} got {score}/{this.props.maxNumberOfQuestions}</p>
+            )
         })
     }
 
@@ -75,7 +105,7 @@ class Quiz extends React.Component{
         return( 
             <div>
                 <p className = 'currentPlayer'>Current Player: {this.state.counter % this.state.numberOfPlayers + 1}</p>
-                <GameBox gameInfo={this.state.data[this.state.index]} calculateScore = {this.calculateScore}/>
+                <GameBox gameInfo={this.state.data[this.state.index]} calculateScore = {this.nextGameState}/>
             </div>) 
     }
 
--- quizilla/src/components/Quiz/Quiz.test.js
+++ quizilla/src/components/Quiz/Quiz.test.js
@@ -15,40 +15,32 @@ describe('Quiz.js', () => {
 
     beforeEach(() => {
       wrapper= shallow(<Quiz players={"1"}/>);
-    })
+    });
 
     it('should update state for number of players on mount', ()=> {
         const instance = mount(<Quiz players={"1"}/>).instance();
         const spy = jest.spyOn(instance, 'createScoreArray');
         instance.componentDidMount();
         expect(spy).toHaveBeenCalled();
-    })
+    });
     
     
     it('should have a div', () => {
         expect(wrapper.find('div').length).toEqual(1);
-    })
+    });
     
     it('should not render GameBox', () => {
         expect(wrapper.find('GameBox').length).toEqual(0);
-    })
+    });
     
     it('should match snapshot', () => {
         const tree = renderer.create(<Quiz players={"1"} />).toJSON();
         expect(tree).toMatchSnapshot();
-    })
-
-    // // We must call spyOn before we shallow render or mount
-    // it('should do an api call', () 
-     
-    //     const getSpy = jest.spyOn(axios, 'get');
-    //     wrapper= shallow(<Quiz />);
-    //     expect(getSpy).toHaveBeenCalled();
-    // })
+    });
     
     it('score state should have a length of 1', () => {
         expect(wrapper.state().score.length).toEqual(1);
-    })
+    });
 
     it('testing GameStates if statement', () => {
         let wrapper2 = shallow(<Quiz players={'1'} maxNumberOfQuestions={1}/>)
@@ -56,7 +48,7 @@ describe('Quiz.js', () => {
         wrapper2.update();
         wrapper2.setState({index: 1 });
         expect(fakeFxn).toHaveBeenCalled();
-    })
+    });
 
     it('testing GameStates else statement', () => {
         let wrapper3 = shallow(<Quiz players={'1'} maxNumberOfQuestions={1}/>)
@@ -64,23 +56,92 @@ describe('Quiz.js', () => {
         wrapper3.update();
         wrapper3.setState({data: [0, 0] });
         expect(fakeFxn).toHaveBeenCalled();
-    })
+    });
 
-    it('should render emojis', () => {
-        let wrapper3 = shallow(<Quiz players={'1'} maxNumberOfQuestions={1}/>)
-        wrapper3.setState({score: [0]});
-        wrapper3.instance().generateResults();
-        expect(wrapper3.find('.resultsMsg').text()).toEqual('Player 1 got 0/1')
-    })
+    it('should return + 1 if the answer is correct', () =>{
+        let wrapper3 = shallow(<Quiz players={'1'} maxNumberOfQuestions={1}/>);
+        let mockData = [{
+            "category": "Mythology",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Talos, the mythical giant bronze man, was the protector of which island?",
+            "correct_answer": "Crete",
+            "incorrect_answers": [
+                "Sardinia",
+                "Sicily",
+                "Cyprus"
+            ]
+        }];
+        wrapper3.setState({score: [0], counter: 0, numberOfPlayers: 1, data: mockData, index: 0});
+        wrapper3.update();
+        let fakeCalcScore = wrapper3.instance().calculateScore ('Crete');
+        expect(fakeCalcScore[0]).toEqual(1);
+    });
 
-    // it('testing GameStates if both if and else statement are false', () => {
-    //     let wrapper4 = shallow(<Quiz players={'1'} maxNumberOfQuestions={1}/>)
-    //     let fakeFxn = wrapper3.instance().generateNextQuestion = jest.fn();
-    //     wrapper3.update();
-    //     wrapper3.setState({data: [0, 0] });
-    //     expect(fakeFxn).toHaveBeenCalled();
-    // })
+    it('should return + 1 if the answer is correct', () =>{
+        let wrapper3 = shallow(<Quiz players={'1'} maxNumberOfQuestions={1}/>);
+        let mockData = [{
+            "category": "Mythology",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Talos, the mythical giant bronze man, was the protector of which island?",
+            "correct_answer": "Crete",
+            "incorrect_answers": [
+                "Sardinia",
+                "Sicily",
+                "Cyprus"
+            ]
+        }];
+        wrapper3.setState({score: [0], counter: 0, numberOfPlayers: 1, data: mockData, index: 0});
+        wrapper3.update();
+        let fakeCalcScore = wrapper3.instance().calculateScore ('Sicily');
+        expect(fakeCalcScore[0]).toEqual(-1);
+    });
+
+    it('should return 1 if there is one player', () =>{
+        let wrapper3 = shallow(<Quiz players={'1'} maxNumberOfQuestions={1}/>);
+        let fakeUpdateQ = wrapper3.instance().updateQuestion();
+        expect(fakeUpdateQ).toEqual(1);
+    });
+
+    it('should return 0 if there are 2 players and it is the first players turn', () =>{
+        let wrapper3 = shallow(<Quiz players={'2'} maxNumberOfQuestions={1}/>);
+        wrapper3.setState({counter: 0});
+        wrapper3.update();
+        let fakeUpdateQ = wrapper3.instance().updateQuestion();
+        expect(fakeUpdateQ).toEqual(0);
+    });
     
+    it('should return 1 if there are 2 players and it is the second players turn', () =>{
+        let wrapper3 = shallow(<Quiz players={'2'} maxNumberOfQuestions={1}/>);
+        wrapper3.setState({counter: 1});
+        wrapper3.update();
+        let fakeUpdateQ = wrapper3.instance().updateQuestion();
+        expect(fakeUpdateQ).toEqual(1);
+    });
+
+    it('gameState should run newScore, followed by nextQuestion', () =>{
+        let wrapper3 = shallow(<Quiz players={'1'} maxNumberOfQuestions={1}/>);
+        let mockData = [{
+            "category": "Mythology",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Talos, the mythical giant bronze man, was the protector of which island?",
+            "correct_answer": "Crete",
+            "incorrect_answers": [
+                "Sardinia",
+                "Sicily",
+                "Cyprus"
+            ]
+        }];
+        wrapper3.setState({score: [0], counter: 0, numberOfPlayers: 1, data: mockData, index: 0});
+        wrapper3.update();
+        let fakeCalcScore = wrapper3.instance().calculateScore = jest.fn();
+        let fakeUpdateQ = wrapper3.instance().updateQuestion = jest.fn();
+        wrapper3.instance().nextGameState();
+        expect(fakeCalcScore).toHaveBeenCalled();
+        expect(fakeUpdateQ).toHaveBeenCalled();
+    })
 }) 
 
 
